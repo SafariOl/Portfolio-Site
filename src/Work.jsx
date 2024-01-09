5eb16e5e0356f4ed2cc845d668769b1086679a99
Olha <s.olha.vit@gmail.com>
2024-01-09 21:56:36 +0200

    adaptive

--- src/Work.jsx
+++ src/Work.jsx
@@ -7,7 +7,7 @@ export default function Work({titlePage, elem}) {
 
     useEffect(() => {
         ScrollTrigger.matchMedia({
-            "(min-width: 300px)": function(){
+            "(min-width: 982px)": function(){
                 gsap.to('.work__block ', .5, {
                     y: 30,
                     height: 130,
@@ -18,9 +18,27 @@ export default function Work({titlePage, elem}) {
                     scrollTrigger: {
                         trigger: '.work__block',
                         scrub: true,
-                        start: 'top 70%',
+                        start: 'top 80%',
                         end: '+=300'
                     }
+                })
+            }
+        })
+
+        ScrollTrigger.matchMedia({
+            "(max-width: 982px)": function(){
+                const works = document.querySelectorAll('.work__items__block')
+                const worksImages = document.querySelectorAll('.work__image')
+                works.forEach((block, idx) => {
+                    gsap.to(worksImages[idx], 1, {
+                        maxHeight: 370,
+                        scrollTrigger: {
+                            trigger: block,
+                            scrub: true,
+                            start: 'top 60%',
+                            end: '+=300'
+                        }
+                    })
                 })
             }
         })
@@ -43,6 +61,7 @@ export default function Work({titlePage, elem}) {
         })
     })
         
+    
 
     function mouseEnter (order) {
         const image = document.querySelectorAll('.work__image')[order]
@@ -67,6 +86,9 @@ export default function Work({titlePage, elem}) {
                             <h2>FEELING Y</h2>
                         </div>
                     </a>
+                    <div className="work__items__image">
+                        <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da2f_max-andrey-TlRQin0iwjE-unsplash-min.jpg" alt="#" />
+                    </div>
                 </div>
                 <div className='work__items__block'>
                     <a href='/marcomit' onMouseEnter={() => mouseEnter(1)} onMouseLeave={() => mouseLeave(1)} className="work__block">
@@ -75,6 +97,9 @@ export default function Work({titlePage, elem}) {
                             <h2>MARCOMIT</h2>
                         </div>
                     </a>
+                    <div className="work__items__image">
+                        <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da30_martin-katler-rjASNUw3SDE-unsplash-min.jpg" alt="#" />
+                    </div>
                 </div>
                 <div className='work__items__block'>
                     <a href='/ron-busti' onMouseEnter={() => mouseEnter(2)} onMouseLeave={() => mouseLeave(2)} className="work__block">
@@ -83,6 +108,9 @@ export default function Work({titlePage, elem}) {
                             <h2>RON BUSTI</h2>
                         </div>
                     </a>
+                    <div className="work__items__image">
+                        <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da31_trinity-nguyen-_yMRmwr2OY8-unsplash-min.jpg" alt="#" />
+                    </div>
                 </div>
                 <div className='work__items__block'>
                     <a href='/cereal' onMouseEnter={() => mouseEnter(3)} onMouseLeave={() => mouseLeave(3)} className="work__block">
@@ -91,6 +119,9 @@ export default function Work({titlePage, elem}) {
                             <h2>CEREAL</h2>
                         </div>
                     </a>
+                    <div className="work__items__image">
+                        <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da32_sarath-p-raj-p8GmCEgSmmo-unsplash-min.jpg" alt="#" />
+                    </div>
                 </div>
                 <div className='work__items__block'>
                     <a href='/romans' onMouseEnter={() => mouseEnter(4)} onMouseLeave={() => mouseLeave(4)} className="work__block">
@@ -99,14 +130,12 @@ export default function Work({titlePage, elem}) {
                             <h2>ROMANS</h2>
                         </div>
                     </a>
+                    <div className="work__items__image">
+                        <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da33_oguz-yagiz-kara-OhC9aDrGsHU-unsplash-min.jpg" alt="#" />
+                    </div>
                 </div>
             </div>
             <div className="work__images">
-                <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da2f_max-andrey-TlRQin0iwjE-unsplash-min.jpg" alt="#" />
-                <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da30_martin-katler-rjASNUw3SDE-unsplash-min.jpg" alt="#" />
-                <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da31_trinity-nguyen-_yMRmwr2OY8-unsplash-min.jpg" alt="#" />
-                <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da32_sarath-p-raj-p8GmCEgSmmo-unsplash-min.jpg" alt="#" />
-                <img className='work__image' src="https://assets-global.website-files.com/65455474e0ea54684cf7d9cf/65455474e0ea54684cf7da33_oguz-yagiz-kara-OhC9aDrGsHU-unsplash-min.jpg" alt="#" />
             </div>
         </div>
     </div>
